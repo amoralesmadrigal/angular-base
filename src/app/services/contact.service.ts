f29f6a7f4ada0e7c5ebf483b97f8947e0a6f2cea
Adan <kraken01@gmail.com>
2024-06-08 14:34:47 +0200

    cambio de url por render

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -8,8 +8,9 @@ import { Contact } from '../models/contact.model';
 })
 export class ContactService {
 
-  private apiUrl = 'https://demo-production-6c66.up.railway.app/api/contacts/create';
-  //private apiUrl = 'http://localhost:8080/api/contacts/create';
+  //private apiUrl = 'https://demo-production-6c66.up.railway.app/api/contacts/create';
+  private apiUrl = 'https://demo-1s5j.onrender.com/api/contacts/create';
+  //private apiUrl = 'http://localhost:8000/api/contacts/create';
   constructor(private http: HttpClient) { }
 
   createContact(contact: Contact): Observable<void> {
