1484e47c7be7dbba783de53c16963795add801aa
Adan <kraken01@gmail.com>
2024-12-04 17:16:01 +0100

    se agrega configuracion ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import {ApplicationConfig} from '@angular/core'
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { withInterceptorsFromDi, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptorsFromDi(), withFetch()), provideClientHydration()
     ]
 }
